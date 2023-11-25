cca7e0db6d28a6899d954aeaa795da7da8c2828a
WojtekKirsz <w.kirsz@gmail.com>
2023-11-25 16:33:10 +0100

    change

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+import { resolve } from "path";
+
 export default {
   base: "/goit-js-hw-09/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        srcIndex: resolve(__dirname, "src/index.html"),
+        colorSwitcher: resolve(__dirname, "src/01-color-switcher.html"),
+        timer: resolve(__dirname, "src/02-timer.html"),
+        promises: resolve(__dirname, "src/03-promises.html"),
+      },
+    },
+  },
 };
